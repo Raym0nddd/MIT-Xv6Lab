01055cdaefe40ee9f9c5fc2a0644dc2eec08acd3
raymond <raymond4ever@163.com>
2023-07-20 23:22:06 +0800

    修改pingpong

--- user/pingpong.c
+++ user/pingpong.c
@@ -1,30 +1,36 @@
 #include "kernel/types.h"
-#include "kernel/stat.h"
 #include "user/user.h"
 
+#define RD 0
+#define WT 1
 
 int main(int argc, char *argv[])
 {
-    int p[2];
+    int c2p[2], p2c[2];
     char signal[10] ={0};
 
-    pipe(p);
+    pipe(c2p);
+    pipe(p2c);
     if(fork() == 0)
     {
-        read(p[0], signal, 10);
-        printf("<%d>:received %s\n", getpid(), signal);
-        write(p[1], "pong", 10);
-        close(p[0]);
-        close(p[1]);
+        close(c2p[RD]);
+        close(p2c[WT]);
+        read(p2c[RD], signal, 10);
+        printf("%d: received %s\n", getpid(), signal);
+        write(c2p[WT], "pong", 10);
+        close(c2p[WT]);
+        close(p2c[RD]);
     }
     else
     {
-        write(p[1], "ping", 10);
+        close(c2p[WT]);
+        close(p2c[RD]);
+        write(p2c[WT], "ping", 10);
         wait(0);
-        read(p[0], signal, 10);
-        printf("<%d>:received %s\n", getpid(), signal);
-        close(p[0]);
-        close(p[1]);
+        read(c2p[RD], signal, 10);
+        printf("%d: received %s\n", getpid(), signal);
+        close(c2p[RD]);
+        close(p2c[WT]);
     }
 
     exit(0);
